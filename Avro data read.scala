dccbcd87452d7e6d27b41cb9123dd39158c3d5ae
Veeraravi Kumar Singiri <veeraravikumar.singiri@gmail.com>
2024-07-30 15:46:48 +0530

    Update Avro data read.scala

--- Avro data read.scala
+++ Avro data read.scala
@@ -81,3 +81,76 @@ val df = spark.createDataFrame(rowRDD, schema)
 // Show the DataFrame
 df.show()
 
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.serialization.StringDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericRecord
+import org.apache.spark.sql.{SparkSession, Row, DataFrame}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.avro.SchemaConverters
+
+import java.util.Properties
+import scala.collection.JavaConverters._
+
+object KafkaAvroToDataFrame {
+  def main(args: Array[String]): Unit = {
+    // Initialize SparkSession
+    val spark = SparkSession.builder()
+      .appName("KafkaToDataFrame")
+      .getOrCreate()
+
+    // Kafka consumer configuration
+    val props = new Properties()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "your_group_id")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[KafkaAvroDeserializer])
+    props.put("schema.registry.url", "http://localhost:8081")
+    props.put("specific.avro.reader", "false")  // Use GenericRecord
+
+    val consumer = new KafkaConsumer[String, GenericRecord](props)
+    consumer.subscribe(java.util.Arrays.asList("your_topic_name"))
+
+    // Fetch schema from Schema Registry
+    val schemaRegistryUrl = "http://localhost:8081"
+    val topicName = "your_topic_name"
+    val schemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryUrl, 128)
+    val schemaMetadata = schemaRegistryClient.getLatestSchemaMetadata(s"$topicName-value")
+    val avroSchema = new Schema.Parser().parse(schemaMetadata.getSchema)
+    val sparkSchema = SchemaConverters.toSqlType(avroSchema).dataType.asInstanceOf[StructType]
+
+    // Function to convert GenericRecord to Row
+    def genericRecordToRow(record: GenericRecord, schema: Schema): Row = {
+      val values = schema.getFields.toArray.map { field =>
+        val fieldName = field.asInstanceOf[Schema.Field].name()
+        record.get(fieldName) match {
+          case utf8: org.apache.avro.util.Utf8 => utf8.toString
+          case other => other
+        }
+      }
+      Row(values: _*)
+    }
+
+    // Consume messages from Kafka
+    val records = consumer.poll(1000).asScala
+
+    // Convert RDD[ConsumerRecord[String, GenericRecord]] to RDD[Row]
+    val rowRDD = spark.sparkContext.parallelize(records.map(record => genericRecordToRow(record.value(), avroSchema)).toSeq)
+
+    // Create DataFrame
+    val df = spark.createDataFrame(rowRDD, sparkSchema)
+
+    // Show the DataFrame
+    df.show()
+
+    // Stop the SparkSession
+    spark.stop()
+  }
+}
+
+
+
+
